0280663ef4ccdee83eadd9edc3f4e17acb7c05b1
touch studio <ia@touch.com.co>
2025-12-14 19:37:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2340589964-eab24",
+  "appId": "1:136971202868:web:b1764a6ee12b737328f561",
+  "apiKey": "AIzaSyCFYusB-wXvKeDh2rk3t-WOT9H_PK-V4Cc",
+  "authDomain": "studio-2340589964-eab24.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "136971202868"
+};
